24d2da06354e940bcef96057530f53b388183aaf
shnat-shnat <106736968+shnat-shnat@users.noreply.github.com>
2023-01-30 14:31:33 +0300

    task file added
    Добавлен файл с решением задачи

---
+++ task.cs
@@ -0,0 +1,23 @@
+array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+int lenNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) lenNewArray++;
+}
+
+string[] newArray = new string[lenNewArray];
+int idx = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[idx] = array[i];
+        idx++;
+    }
+}
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
